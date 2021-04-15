38dfa6da7b1818e744b4f37a8cbe3f01f38f0059
adarsh sharma <68946988+adarsh-projects@users.noreply.github.com>
2021-04-16 01:08:12 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+from item_colloberative import RecomdModel
+import pandas as pd
+import json
+
+model = RecomdModel()
+
+app = Flask("__name__")
+
+@app.errorhandler(404)
+def not_found(error):
+    return "<h2>404</h2><p>Not found</p>"
+
+@app.route('/')
+def apitest():
+	return render_template('index.html', query="")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	#query = [ x for x in request.form.values()]
+	
+	# user rating sex
+	#value = model.predictModel(int(query[0]), int(query[1]), int(query[2]))
+	value = model.predictModel(180, 3, 327)
+	
+	value = {"brand": [x for x in value[0]]}
+	return value #{"item": "hey Adarsh"}
+	#return {"item": "Hey, Adarsh"}
+
+if __name__ == '__main__':
+	app.run()
